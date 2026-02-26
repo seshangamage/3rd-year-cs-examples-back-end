050a8d9dd126d4b8c2fbe4ff56c759c340129b06
Gamage <sgamage@wiley.com>
2026-02-26 10:18:43 +0530

    Added CORS config

--- Startup.cs
+++ Startup.cs
@@ -8,9 +8,21 @@ namespace StudentMicroservice
 {
     public class Startup
     {
+        private const string FrontendCorsPolicy = "FrontendCorsPolicy";
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<StudentContext>(opt => opt.UseInMemoryDatabase("StudentDb"));
+            services.AddCors(options =>
+            {
+                options.AddPolicy(FrontendCorsPolicy, builder =>
+                {
+                    builder
+                        .WithOrigins("https://purple-tree-0684b981e.6.azurestaticapps.net")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
@@ -26,6 +38,7 @@ namespace StudentMicroservice
             app.UseSwaggerUI();
 
             app.UseRouting();
+            app.UseCors(FrontendCorsPolicy);
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
--- obj/Debug/net9.0/StudentMicroservice.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d57aedaf348535db11f099828a0af8029a0be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3c55227032d0267b3b84f97a606eff7679f502")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
